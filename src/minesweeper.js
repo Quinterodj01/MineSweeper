5ec2be41fcd23aa32da531a1621d97681c24abb1
David Quintero <David.J.Quintero@gmail.com>
2018-06-13 09:42:21 -0500

    code agter learning array and loops

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,4 +1,4 @@
-const printBoard = (board) => {
+/*const printBoard = (board) => {
     console.log('Current board');
     console.log('');
 
@@ -15,4 +15,60 @@ const board = [
 printBoard(board);
 board[0][1] = '1';
 board[2][2] = 'B';
-printBoard(board);
+printBoard(board);*/
+/* Nuew code*/
+
+
+const generatePlayerBoard = (numberOfRows, numberOfcolums) => {
+    let board = [];
+    //add Rows
+    for(let i = 0; i < numberOfRows; i++){
+        //board[i] = " ";
+        let row = [];
+        for(let j = 0; j< numberOfcolums; j++){
+          row.push(' ');
+            //board[i][j] = ' ';
+        }
+        board.push(row);
+    }
+    return board;
+};
+
+const generateBombBoard = (numberOfRows, numberOfcolums,numberOfBombs) => {
+    let board = [];
+    //add Rows
+    for(let i = 0; i < numberOfRows; i++){
+        //board[i] = " ";
+        let row = [];
+        for(let j = 0; j< numberOfcolums; j++){
+          row.push(null);
+            //board[i][j] = ' ';
+        }
+        board.push(row);
+    };
+    //The code in your while loop has the potential to place bombs on top of already existing bombs. This will be fixed when you learn about control flow.
+    let numberOfBombsPlaced = 0;
+    while (numberOfBombsPlaced < numberOfBombs){
+        //let randomRowIndex = Math.floor(Math.random() * Math.floor(numberOfRows));
+       // let ramdomColIndex = Math.floor(Math.random()*Math.floor(numberOfcolums));
+        board[Math.floor(Math.random() * Math.floor(numberOfRows))][Math.floor(Math.random()*Math.floor(numberOfcolums))] = 'B';
+        numberOfBombsPlaced++;
+    }
+    
+    
+    
+    return board;
+};
+const printBoard = board =>{
+
+   console.log(board.map(row => 
+              row.join(' | ')).join('\n'));
+};
+
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+console.log('Player Board');
+printBoard(playerBoard);
+
+console.log('Bomb Board');
+printBoard(bombBoard);
